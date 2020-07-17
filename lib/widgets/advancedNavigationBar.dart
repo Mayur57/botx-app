cb8c3b2fc0ea25ac7a9cf88672862f77dd9cf61d
Mayur Bhoi <mayur072000@gmail.com>
2020-07-17 19:45:07 +0530

    Added Bot State Toast

--- lib/widgets/advancedNavigationBar.dart
+++ lib/widgets/advancedNavigationBar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class AdvancedNavBar extends StatefulWidget {
   @override
@@ -9,6 +10,87 @@ class _AdvancedNavBarState extends State<AdvancedNavBar> {
   final EdgeInsetsGeometry _iconPadding = EdgeInsets.all(8.0);
   final double _iconSize = 32;
   final Color _iconColor = Colors.white;
+  FToast fToast;
+  bool deviceState = false;
+
+  @override
+  void initState() {
+    super.initState();
+    fToast = FToast(context);
+  }
+
+  _showToast() {
+    Widget toast = deviceState
+        ? Container(
+            //Device unlocked
+            width: 500,
+            height: 110,
+            padding: const EdgeInsets.symmetric(horizontal: 0.0, vertical: 0.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16.0),
+              color: Colors.greenAccent.withOpacity(0.9),
+            ),
+            child: Center(
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(
+                    Icons.lock_open,
+                    size: 30,
+                  ),
+                  SizedBox(
+                    width: 12.0,
+                  ),
+                  Text(
+                    "Device Unlocked",
+                    style: TextStyle(fontSize: 25),
+                  ),
+                ],
+              ),
+            ),
+          )
+        : Container(
+            //Device Locked
+            width: 500,
+            height: 110,
+            padding: const EdgeInsets.symmetric(horizontal: 0.0, vertical: 0.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16.0),
+              color: Colors.redAccent.withOpacity(0.9),
+            ),
+            child: Center(
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(
+                    Icons.lock_outline,
+                    size: 30,
+                  ),
+                  SizedBox(
+                    width: 12.0,
+                  ),
+                  Text(
+                    "Device Locked",
+                    style: TextStyle(fontSize: 25),
+                  ),
+                ],
+              ),
+            ),
+          );
+
+    fToast.showToast(
+      child: toast,
+      gravity: ToastGravity.CENTER,
+      toastDuration: Duration(seconds: 2),
+    );
+
+    //Toggle state after device toast is shown
+    toggleUnlockState();
+  }
+
+  toggleUnlockState() {
+    deviceState = !deviceState;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +108,9 @@ class _AdvancedNavBarState extends State<AdvancedNavBar> {
                   size: _iconSize,
                   color: _iconColor,
                 ),
-                onPressed: () {}, //TODO: Change page here
+                onPressed: () {
+                  _showToast();
+                }, //TODO: Change page here
               ),
             ),
           ),
