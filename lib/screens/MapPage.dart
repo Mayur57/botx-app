e3174157b8900bb72d629c9dd2034f164d0f67a3
Kautubh Sharma <rrjk.sharma@gmail.com>
2020-07-18 20:28:49 +0530

    Current location as origin input + point specification

--- lib/screens/MapPage.dart
+++ lib/screens/MapPage.dart
@@ -1,44 +1,48 @@
 import 'dart:async';
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import '../globals/api.dart' as global;
 import 'package:botx/requests/google_maps_requests.dart';
 import 'package:botx/widgets/advancedNavigationBar.dart';
 import 'package:botx/widgets/centerFloatingActionButton.dart';
 import 'package:botx/widgets/customSearchBar.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 
 //TODO: Save all strings into a different file : strings.dart
 
 class MapPage extends StatefulWidget {
   @override
-  _MapPageState createState() => _MapPageState();
+  MapPageState createState() => MapPageState();
 }
 
 //Hi this was changed
-class _MapPageState extends State<MapPage> {
+class MapPageState extends State<MapPage> {
   Timer _timer;
   bool delayTimeoutConstant = false;
   var isGpsEnabled = Geolocator().isLocationServiceEnabled();
-  static LatLng _initialPosition;
+  //static LatLng _initialPosition;
   Completer<GoogleMapController> _controller = Completer();
-  Map<PolylineId, Polyline> polyLines = {};
   GoogleMapController _mapController;
   PolylinePoints polylinePoints = PolylinePoints();
-  String route;
-  Set<Marker> myMarker = {};
-  MapType _currentMapType = MapType.normal;
+  //LatLng initialPosition;
+  //Map<MarkerId, Marker> myMarkers = <MarkerId, Marker>{};
+  //Set<Marker> myMarker = {};
+
 
   @override
   void initState() {
     super.initState();
-    _getUserLocation();
+    getUserLocation();
     _setTimer();
   }
 
   _setTimer() {
-    _timer = new Timer(Duration(milliseconds: 2000), () { //Timeout is set to 2 seconds for the location warning
+    _timer = new Timer(Duration(milliseconds: 2000), () {
+      //Timeout is set to 2 seconds for the location warning
       setState(() {
         delayTimeoutConstant = true;
       });
@@ -53,7 +57,7 @@ class _MapPageState extends State<MapPage> {
 
   @override
   Widget build(BuildContext context) {
-    return _initialPosition == null
+    return global.initialPosition == null
         ? Scaffold(
             backgroundColor: Colors.white,
             body: Stack(
@@ -66,8 +70,8 @@ class _MapPageState extends State<MapPage> {
                       Container(
                           height: 70,
                           width: 70,
-                          child: CircularProgressIndicator(
-                            strokeWidth: 6,
+                          child: SpinKitWave(
+                            color: Colors.blueAccent,
                           )),
                       SizedBox(
                         height: 20,
@@ -89,13 +93,14 @@ class _MapPageState extends State<MapPage> {
                     padding: const EdgeInsets.symmetric(horizontal: 30),
                     child: Container(
                       height: 70,
-                      child: delayTimeoutConstant ? Text(
-                        "Important: If you see this screen for more than 5 seconds, please check your Device Location settings and enable Location Services with location accuracy set to HIGH ACCURACY.",
-                        textAlign: TextAlign.justify,
-                        style: TextStyle(fontSize: 11),
-                      )
-                      :  Text(" "),
-                  ),
+                      child: delayTimeoutConstant
+                          ? Text(
+                              "Important: If you see this screen for more than 5 seconds, please check your Device Location settings and enable Location Services with location accuracy set to HIGH ACCURACY.",
+                              textAlign: TextAlign.justify,
+                              style: TextStyle(fontSize: 11),
+                            )
+                          : Text(" "),
+                    ),
                   ),
                 )
               ],
@@ -109,14 +114,13 @@ class _MapPageState extends State<MapPage> {
                   compassEnabled: false,
                   zoomControlsEnabled: false,
                   myLocationEnabled: true,
-                  mapType: _currentMapType,
                   tiltGesturesEnabled: true,
                   initialCameraPosition:
-                      CameraPosition(target: _initialPosition, zoom: 16),
+                      CameraPosition(target: global.initialPosition, zoom: 16),
                   onMapCreated: _onMapCreated,
-                  onLongPress: _Marker,
-                  markers: myMarker,
-                  polylines: Set<Polyline>.of(polyLines.values),
+                  onLongPress: marker,
+                  markers: global.myMarker.values.toSet(),
+                  polylines: global.polyLines.values.toSet(),
                 ),
                 //MapToogleFAB(),
                 ///Disabled due to developer request
@@ -137,53 +141,101 @@ class _MapPageState extends State<MapPage> {
   }
 
   // ignore: non_constant_identifier_names
-  _Marker(LatLng tappedPoint) async {
-    myMarker = {};
-    setState(() {
-      myMarker.add(
-        Marker(
-          markerId: MarkerId(tappedPoint.toString()),
-          position: tappedPoint,
-          draggable: false,
-          infoWindow: InfoWindow(
-            title: tappedPoint.toString(),
-            snippet: 'Cool place',
-          ),
-        ),
-      );
-      _getPolyline(tappedPoint);
-    });
+
+  marker(LatLng tappedPoint) async {
+    switch (global.pickUpType) {
+      case 1:
+        setState(() {
+          final marker2 = Marker(
+              markerId: MarkerId(tappedPoint.toString()),
+              position: tappedPoint,
+              infoWindow: InfoWindow(
+                title: "Destination",
+              ));
+          global.myMarker[2] = marker2;
+          print("##########################################");
+          getPolyline(global.initialPosition, tappedPoint);
+        });
+        break;
+      case 2:
+        setState(() {
+          final marker2 = Marker(
+              markerId: MarkerId(tappedPoint.toString()),
+              position: tappedPoint,
+              infoWindow: InfoWindow(
+                title: "Destination",
+              ));
+          global.initialPosition = global.currentLocation;
+          global.myMarker[2] = marker2;
+          print("##########################################");
+          getPolyline(global.initialPosition, tappedPoint);
+        });
+        break;
+      case 3:
+        setState(() {
+          final marker1 = Marker(
+              markerId: MarkerId(tappedPoint.toString()),
+              position: tappedPoint,
+              infoWindow: InfoWindow(
+                title: "Pick-Up",
+              ));
+          global.myMarker[1] = marker1;
+          print("==========================================");
+          global.pickUpType = 1;
+          global.initialPosition = tappedPoint;
+        });
+        break;
+      default:
+        setState(() {
+          final marker2 = Marker(
+              markerId: MarkerId(tappedPoint.toString()),
+              position: tappedPoint,
+              infoWindow: InfoWindow(
+                title: "Destination",
+              ));
+          global.myMarker[2] = marker2;
+          print("##########################################");
+          getPolyline(global.initialPosition, tappedPoint);
+        });
+        break;
+    }
   }
 
-  void _getUserLocation() async {
+  void getUserLocation() async {
     Position position = await Geolocator()
         .getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
     setState(() {
-      _initialPosition = LatLng(position.latitude, position.longitude);
+      global.currentLocation = LatLng(position.latitude, position.longitude);
+      global.initialPosition = global.currentLocation;
     });
   }
 
-  _getPolyline(LatLng destination) async {
+  getPolyline(LatLng source, LatLng destination) async {
     List<LatLng> polylineCoordinates = [];
     PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
       apikey,
-      PointLatLng(_initialPosition.latitude, _initialPosition.longitude),
+      PointLatLng(source.latitude, source.longitude),
       PointLatLng(destination.latitude, destination.longitude),
+      travelMode: TravelMode.driving,
     );
     if (result.points.isNotEmpty) {
       result.points.forEach((PointLatLng point) {
         polylineCoordinates.add(LatLng(point.latitude, point.longitude));
       });
     }
-    _addPolyLine(polylineCoordinates);
+    addPolyLine(polylineCoordinates);
   }
 
-  _addPolyLine(List<LatLng> cords) {
+  addPolyLine(List<LatLng> cords) {
     PolylineId id = PolylineId("Poly");
     Polyline polyline =
         Polyline(polylineId: id, color: Colors.blueAccent, points: cords);
-    polyLines[id] = polyline;
-
+    global.polyLines[id] = polyline;
+    print("----------------POLYLINE---------------------");
+    print(global.polyLines.values.toSet().toString());
     setState(() {});
   }
 }
+
+
+
--- lib/widgets/sourceTextField.dart
+++ lib/widgets/sourceTextField.dart
@@ -1,35 +1,84 @@
+import 'package:botx/screens/MapPage.dart';
+import '../globals/api.dart' as global;
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_webservice/places.dart';
+import 'package:flutter_google_places/flutter_google_places.dart';
 
 class SourceTextField extends StatefulWidget {
   @override
-  _SourceTextFieldState createState() => _SourceTextFieldState();
+  SourceTextFieldState createState() => SourceTextFieldState();
 }
 
-class _SourceTextFieldState extends State<SourceTextField> {
+class SourceTextFieldState extends State<SourceTextField> {
+  MapPageState myMap = MapPageState();
+  TextEditingController source = TextEditingController();
+  int state;
   @override
   Widget build(BuildContext context) {
     return Row(
       children: <Widget>[
         Expanded(
           child: TextFormField(
+            onTap: () async {
+              Prediction p = await PlacesAutocomplete.show(
+                  context: context,
+                  apiKey: global.googleApiKey,
+                  language: "en",
+                  components: [Component(Component.country, "in")]);
+              if (p != null) {
+                source.text = p.description;
+              }
+            },
+            controller: source,
             decoration: InputDecoration(
               border: InputBorder.none,
               hintText: 'Source',
-              hintStyle: TextStyle(
-                  fontSize: 16.0, color: Colors.grey),
+              hintStyle: TextStyle(fontSize: 16.0, color: Colors.grey),
             ),
           ),
         ),
         IconButton(
-          icon: Icon(Icons.my_location, color: Colors.white60,),
+          icon: Icon(
+            Icons.my_location,
+            color: Colors.white60,
+          ),
           onPressed: () {
-            print("Select my location");
+            state = 2;
+            setState(() {
+              print("====================CURRENT LOCATION===================");
+              global.polyLines.remove("Poly");
+              global.myMarker.remove(1);
+              global.myMarker.remove(2);
+              global.myMarker.clear();
+              global.polyLines.clear();
+              print(global.pickUpType);
+              global.initialPosition = global.currentLocation;
+              global.pickUpType = state;
+              print("Select my location");
+              print(global.pickUpType);
+            });
           },
         ),
         IconButton(
-          icon: Icon(Icons.location_on,color: Colors.white60,),
+          icon: Icon(
+            Icons.location_on,
+            color: Colors.white60,
+          ),
           onPressed: () {
-            print("Select marker on map");
+            state = 3;
+            setState(() {
+              print("=====================MARKER========================");
+              global.polyLines.remove("Poly");
+              global.myMarker.remove(1);
+              global.myMarker.remove(2);
+              global.myMarker.clear();
+              global.polyLines.clear();
+              print(global.pickUpType);
+              global.pickUpType = state;
+              print("Select marker on map");
+              print(global.pickUpType);
+            });
           },
         ),
       ],
