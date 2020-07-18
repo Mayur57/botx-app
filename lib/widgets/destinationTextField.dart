fe61f5a312675e2e8bd61f1803ba87e1d0932933
1KS6 <45872335+1KS6@users.noreply.github.com>
2020-07-18 20:31:59 +0530

    Current location as origin input + point specification (#13)
    * Updating api key for ios
    
    * Update AppDelegate.swift
    
    * Update Info.plist
    
    * Updating experimental  (#8) (#2)
    
    * Custom Map Style
    
    * Updating API key for iOS (#7)
    
    * Update AppDelegate.swift
    
    * Update Info.plist
    
    Co-authored-by: Mayur Bhoi <mayur072000@gmail.com>
    
    Co-authored-by: Mayur Bhoi <mayur072000@gmail.com>
    
    * Minor Improvements (#3)
    
    * Updating API key for iOS (#7)
    
    * Update AppDelegate.swift
    
    * Update Info.plist
    
    * Minor improvements to the code
    
    Co-authored-by: Mayur Bhoi <mayur072000@gmail.com>
    
    * refactoring
    
    * map ui (#4)
    
    * Updating API key for iOS (#7)
    
    * Update AppDelegate.swift
    
    * Update Info.plist
    
    * Minor improvements to the code
    
    * Search Bar Improvements
    
    * Added Pages and Routing Functionality
    
    * Added Bot State Toast
    
    Co-authored-by: Mayur Bhoi <mayur072000@gmail.com>
    
    * Experimental (#6)
    
    * Branch Merge (#12)
    
    * Updating api key for ios
    
    * Update AppDelegate.swift
    
    * Update Info.plist
    
    * Updating experimental  (#8) (#2)
    
    * Custom Map Style
    
    * Updating API key for iOS (#7)
    
    * Update AppDelegate.swift
    
    * Update Info.plist
    
    Co-authored-by: Mayur Bhoi <mayur072000@gmail.com>
    
    Co-authored-by: Mayur Bhoi <mayur072000@gmail.com>
    
    * Minor Improvements (#3)
    
    * Updating API key for iOS (#7)
    
    * Update AppDelegate.swift
    
    * Update Info.plist
    
    * Minor improvements to the code
    
    Co-authored-by: Mayur Bhoi <mayur072000@gmail.com>
    
    * refactoring
    
    * map ui (#4)
    
    * Updating API key for iOS (#7)
    
    * Update AppDelegate.swift
    
    * Update Info.plist
    
    * Minor improvements to the code
    
    * Search Bar Improvements
    
    * Added Pages and Routing Functionality
    
    * Added Bot State Toast
    
    Co-authored-by: Mayur Bhoi <mayur072000@gmail.com>
    
    Co-authored-by: Kautubh Sharma <rrjk.sharma@gmail.com>
    Co-authored-by: 1KS6 <45872335+1KS6@users.noreply.github.com>
    
    * Remote Test For GitHub
    
    Co-authored-by: Mayur Bhoi <mayur072000@gmail.com>
    
    * Current location as origin input + point specification
    
    Co-authored-by: Mayur Bhoi <mayur072000@gmail.com>

--- lib/widgets/destinationTextField.dart
+++ lib/widgets/destinationTextField.dart
@@ -1,4 +1,11 @@
+import 'package:botx/screens/MapPage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_webservice/places.dart' as gws;
+import 'package:flutter_google_places/flutter_google_places.dart';
+import '../globals/api.dart' as global;
 
 class DestinationTextField extends StatefulWidget {
   @override
@@ -6,21 +13,43 @@ class DestinationTextField extends StatefulWidget {
 }
 
 class _DestinationTextFieldState extends State<DestinationTextField> {
+  TextEditingController destination = TextEditingController();
+
+  //final MapPageState _myMap = MapPageState.instance;
+  //MapPageState myMap = MapPageState();
+
+  LatLng dest;
+  int state;
+  PolylinePoints polylinePoints = PolylinePoints();
+
   @override
   Widget build(BuildContext context) {
     return Row(
       children: <Widget>[
         Expanded(
           child: TextFormField(
+            onTap: () async {
+              gws.Prediction p = await PlacesAutocomplete.show(
+                  context: context,
+                  apiKey: global.googleApiKey,
+                  language: "en",
+                  components: [gws.Component(gws.Component.country, "in")]);
+              if (p != null) {
+                destination.text = p.description;
+              }
+            },
+            controller: destination,
             decoration: InputDecoration(
               border: InputBorder.none,
               hintText: 'Destination',
-              hintStyle: TextStyle(
-                  fontSize: 16.0, color: Colors.grey),
+              hintStyle: TextStyle(fontSize: 16.0, color: Colors.grey),
             ),
           ),
         ),
       ],
     );
   }
-}
+
+
+  }
+
